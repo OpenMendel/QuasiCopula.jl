682f15a7eb2048cb8fd20afb1037cfb2251089a4
Sarah Ji <smji@g.ucla.edu>
2021-05-04 20:28:00 -0700

    put test files in folders

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,23 @@
 module PkgTest
 # # # # simulating data
-include("rand_continuous.jl")
-include("rand_discrete.jl")
+include("simulation/rand_continuous.jl")
+include("simulation/rand_discrete.jl")
 
 # # # # simulating random vector using conditional densities
-include("rand_multivariate.jl")
+include("simulation/rand_multivariate.jl")
 
 # # # # analyze simulated data
-include("bivariate_poisson_test.jl")
-include("bivariate_normal_test.jl")
+include("simulation/bivariate_poisson_test.jl")
+include("simulation/bivariate_normal_test.jl")
 
 # # # trivariate poisson test
-include("trivariate_poisson.jl")
+include("simulation/trivariate_poisson.jl")
 
 # # # using mixed models to simulate data + checking gradient and hessian pieces using forward diff
-include("simulate_logistic_mixedmodels.jl")
-include("simulate_poisson_mixedmodels.jl")
+include("estimation/simulate_logistic_mixedmodels.jl")
+include("estimation/simulate_poisson_mixedmodels.jl")
 
 # # example datasets
-include("dyestuff.jl")
+include("examples/dyestuff.jl")
 # include("sleepstudy.jl")
 end
